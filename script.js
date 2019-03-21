0131c4274c92827ae5beb21d8c66b77c7018f671
Kritos33 <47435068+Kritos33@users.noreply.github.com>
2019-03-21 14:54:12 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+//Spell #1
+
+$(".JO").click(function() {
+    $(".white").fadeToggle();
+});
+
+
+//Spell #2
+
+$(".JE").click(function() {
+    $(".black").toggle();
+});
+
+
+//Spell #3
+
+$(".JB").click(function() {
+    $(".green").slideToggle();
+});
